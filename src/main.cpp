c9b3209bf3517a37d8f0f9f0324672e0ced828b3
Magnus Burenius <magnus.burenius@volumental.com>
2023-12-14 15:14:39 +0100

    draw cube in 3D

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 
 #include <vector>
 
+#include <Eigen/Core>
+#include <SDL2/SDL.h>
+
+#include "algorithm.hpp"
+#include "camera.hpp"
 #include "input.hpp"
 #include "window.hpp"
-#include <SDL2/SDL.h>
 
 uint32_t packColorRgb(uint32_t r, uint32_t g, uint32_t b) {
     return (255 << 24) | (r << 16) | (g << 8) | (b << 0);
@@ -14,16 +18,36 @@ int main(int, char**) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         handleSdlError("SDL_Init");
     }
-    const auto WIDTH = 320;
-    const auto HEIGHT = 200;
-    const auto NUM_PIXELS = WIDTH * HEIGHT;
+    auto WIDTH = 320;
+    auto HEIGHT = 200;
+    auto NUM_PIXELS = WIDTH * HEIGHT;
     auto window = makeFullScreenWindow(WIDTH, HEIGHT, "Voxel Landscape");
-    auto pixels = std::vector<uint32_t>(NUM_PIXELS, packColorRgb(0, 0, 255));
+    auto pixels = std::vector<uint32_t>(NUM_PIXELS);
     SDL_ShowCursor(SDL_DISABLE);
+    auto intrinsics = makeCameraIntrinsics(WIDTH, HEIGHT);
+    auto extrinsics = CameraExtrinsics{};
+    extrinsics.z = -10;
+
+    auto points = Vectors4d{
+        {1,1,1,1},{1,-1,1,1},{-1,-1,1,1},{-1,1,1,1},
+        {1,1,-1,1},{1,-1,-1,1},{-1,-1,-1,1},{-1,1,-1,1},
+    };
+
     for (;;) {
         registerFrameInput(window.renderer);
         if (hasReceivedQuitEvent() || isKeyDown(SDL_SCANCODE_ESCAPE)) {
             break;
+        }
+
+        fill(pixels, packColorRgb(0, 0, 0));
+        auto image_from_world = (imageFromCamera(intrinsics) * cameraFromWorld(extrinsics)).eval();
+        for (auto point_in_world : points) {
+            auto point_in_image = (image_from_world * point_in_world).eval();
+            auto u = int(point_in_image.x() / point_in_image.w());
+            auto v = int(point_in_image.y() / point_in_image.w());
+            if (0 <= u && u < WIDTH && 0 <= v && v < HEIGHT) {
+                pixels[v * WIDTH + u] = packColorRgb(255, 255, 255);
+            }
         }
 
         drawPixels(window, pixels.data());
