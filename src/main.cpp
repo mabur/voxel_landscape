49ab64123f0164e19b91fdf917343aa85c278ed0
Magnus Burenius <magnus.burenius@gmail.com>
2023-12-31 18:56:39 +0100

    avoid ball jitter

--- src/main.cpp
+++ src/main.cpp
@@ -126,7 +126,7 @@ Ball updateBall(Ball ball, Image height_map) {
 
 Player updateCamera(Player player) {
     Matrix4d world_from_camera = worldFromCamera(player.extrinsics);
-    Vector4d offset_in_camera = {0, -20, -30, 0};
+    Vector4d offset_in_camera = {0.1, -20, -30, 0};
     Vector4d offset_in_world = world_from_camera * offset_in_camera;
     Vector4d camera_in_world = player.ball.position_in_world + offset_in_world;
     player.extrinsics.x = camera_in_world.x();
